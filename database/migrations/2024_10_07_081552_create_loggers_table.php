bb5f5a4f82cabe20f2cd5b21fa8d8947863c3ac4
swiftercode2 <swifter.code@sikaram.tech>
2024-10-07 13:28:13 +0500

    api logger

---
+++ database/migrations/2024_10_07_081552_create_loggers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activity_log', function (Blueprint $table) {
+            $table->id();
+            $table->text('path')->nullable();
+            $table->text('method')->nullable();
+            $table->text('ip')->nullable();
+            $table->text('http_version')->nullable();
+            $table->longText('headers')->nullable();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->longText('request')->nullable();
+            $table->longText('response')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activity_log');
+    }
+};
